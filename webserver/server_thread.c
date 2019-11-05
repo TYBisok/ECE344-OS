64b1c2afb786397aacfff758030fcd092a137abf
Addisalem Semagn <addis.semagn@mail.utoronto.ca>
2019-11-05 01:57:07 -0500

    finished lab 4

--- webserver/server_thread.c
+++ webserver/server_thread.c
@@ -1,21 +1,33 @@
 #include "request.h"
 #include "server_thread.h"
 #include "common.h"
+#include <pthread.h>
+
+// global vars
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t full = PTHREAD_COND_INITIALIZER; 
+pthread_cond_t empty = PTHREAD_COND_INITIALIZER; 
 
 struct server {
-	int nr_threads;
-	int max_requests;
-	int max_cache_size;
-	int exiting;
-	/* add any other parameters you need */
+	int nr_threads; // # worker threads
+	int max_requests; // buffer size
+	int max_cache_size; // for lab 5
+	int exiting; // ?
+	pthread_t** worker_threads;
+
+    int* buffer; // file descriptors are ints
+	int in;
+	int out;
+	int request_counter;
 };
 
 /* static functions */
+void worker (struct server *sv);
+void send_to_buffer(struct server *sv, int connfd);
+int read_from_buffer(struct server *sv);
 
 /* initialize file data */
-static struct file_data *
-file_data_init(void)
-{
+static struct file_data * file_data_init(void) {
 	struct file_data *data;
 
 	data = Malloc(sizeof(struct file_data));
@@ -26,17 +38,13 @@ file_data_init(void)
 }
 
 /* free all file data */
-static void
-file_data_free(struct file_data *data)
-{
+static void file_data_free(struct file_data *data) {
 	free(data->file_name);
 	free(data->file_buf);
 	free(data);
 }
 
-static void
-do_server_request(struct server *sv, int connfd)
-{
+static void do_server_request(struct server *sv, int connfd) {
 	int ret;
 	struct request *rq;
 	struct file_data *data;
@@ -65,51 +73,126 @@ out:
 
 /* entry point functions */
 
-struct server *
-server_init(int nr_threads, int max_requests, int max_cache_size)
-{
+struct server * server_init(int nr_threads, int max_requests, int max_cache_size) {
 	struct server *sv;
+
+	pthread_mutex_lock(&lock);
 
 	sv = Malloc(sizeof(struct server));
 	sv->nr_threads = nr_threads;
 	sv->max_requests = max_requests;
 	sv->max_cache_size = max_cache_size;
 	sv->exiting = 0;
+        
+	
+	sv->worker_threads = NULL;
+
+    sv->buffer = NULL;
+	sv->in = 0;
+	sv->out = 0;
+	sv->request_counter = 0;
 	
 	if (nr_threads > 0 || max_requests > 0 || max_cache_size > 0) {
-		TBD();
+		if(max_requests > 0) 	/* Lab 4: create queue of max_request size when max_requests > 0 */
+			sv->buffer = (int*)malloc((max_requests + 1) * sizeof(int));
+
+		if(nr_threads > 0) { 	/* Lab 4: create worker threads when nr_threads > 0 */
+			sv->worker_threads = (pthread_t**)malloc(nr_threads * sizeof(pthread_t*));
+			for(int i = 0; i < nr_threads; i++) {
+				sv->worker_threads[i] = (pthread_t*)malloc(sizeof(pthread_t));
+				pthread_create(sv->worker_threads[i], NULL, (void*)&worker, (void*)sv);
+			}
+		}
+		/* Lab 5: init server cache and limit its size to max_cache_size */
 	}
 
-	/* Lab 4: create queue of max_request size when max_requests > 0 */
-
-	/* Lab 5: init server cache and limit its size to max_cache_size */
-
-	/* Lab 4: create worker threads when nr_threads > 0 */
-
+	pthread_mutex_unlock(&lock);
 	return sv;
 }
 
-void
-server_request(struct server *sv, int connfd)
-{
-	if (sv->nr_threads == 0) { /* no worker threads */
-		do_server_request(sv, connfd);
-	} else {
-		/*  Save the relevant info in a buffer and have one of the
-		 *  worker threads do the work. */
-		TBD();
+void worker (struct server *sv) {
+	while(1) {
+		pthread_mutex_lock(&lock);
+
+		while(sv->request_counter == 0) { // if its empty, wait
+			pthread_cond_wait(&empty, &lock);
+                        
+			if(sv->exiting) {
+				pthread_mutex_unlock(&lock);
+				pthread_exit(NULL);
+			}
+		}
+		
+		int connfd = read_from_buffer(sv);
+		
+		pthread_mutex_unlock(&lock);
+         
+		do_server_request(sv, connfd); // process request
 	}
 }
 
-void
-server_exit(struct server *sv)
-{
+int read_from_buffer(struct server *sv) {
+	int connfd = sv->buffer[sv->out]; // read the fd
+
+	if (sv->request_counter == sv->max_requests) // if queue is full, broadcast to all workers waiting
+		pthread_cond_broadcast(&full);
+
+	sv->out = (sv->out + 1) % sv->max_requests; // increment out index 
+	sv->request_counter--; // decrement buffer size
+	return connfd;
+}
+
+// connfd is the socket descriptor the server will use to send data to the client
+void server_request(struct server *sv, int connfd) {
+	if (sv->nr_threads == 0) { /* no worker threads */
+		do_server_request(sv, connfd);
+    }
+	else {
+		pthread_mutex_lock(&lock);
+		while (sv->request_counter == sv->max_requests) {// buffer full 
+			pthread_cond_wait(&full, &lock);
+			if(sv->exiting) {
+				pthread_mutex_unlock(&lock);
+				pthread_exit(NULL);
+			}
+		}
+                
+		send_to_buffer(sv, connfd);
+
+		pthread_mutex_unlock(&lock);
+    }
+}
+
+void send_to_buffer(struct server *sv, int connfd) {
+	sv->buffer[sv->in] = connfd; // add to buffer
+
+	if(sv->request_counter == 0) // if it's empty
+		pthread_cond_broadcast(&empty); // broadcast to all workers waiting for it not to be 
+
+	sv->in = (sv->in+1) % sv->max_requests; // incremement in index
+	sv->request_counter++; // size of buffer
+}
+
+void server_exit(struct server *sv) {
 	/* when using one or more worker threads, use sv->exiting to indicate to
 	 * these threads that the server is exiting. make sure to call
 	 * pthread_join in this function so that the main server thread waits
 	 * for all the worker threads to exit before exiting. */
-	sv->exiting = 1;
 
-	/* make sure to free any allocated resources */
+	sv->exiting = 1;
+	pthread_cond_broadcast(&empty);
+	pthread_cond_broadcast(&full);
+	
+	for (int i = 0; i < sv->nr_threads; i++) {
+		pthread_join(*(sv->worker_threads[i]), NULL);
+		free(sv->worker_threads[i]);
+	}
+
+	if (sv->max_requests > 0) 
+		free(sv->buffer);
+	if (sv->nr_threads > 0) 
+		free(sv->worker_threads);
+
 	free(sv);
+	return; 
 }
